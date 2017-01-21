b4938662a6a09d19c394ec46fdbc51dd841501bd
JustAddAcid <rap4491@yandex.ru>
2017-01-21 12:33:21 +0300

    automatic copy selected text

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function removeLock(){
     document.onselectstart = null;
 
@@ -21,13 +23,59 @@ function removeLock(){
         };
 }
 
+
+function autoCopy(){
+    var inPageScript = '('+function (){
+        var onSelectEnd = function(){
+            if (!window.getSelection) {
+                return;
+            };
+
+            var event = document.createEvent('Event');
+            event.initEvent('selectend', true, true);
+
+            var selectstart = false;
+            document.addEventListener('selectstart', function() {
+                selectstart = true;
+            });
+
+            document.addEventListener('mouseup', function() {
+                raiseEvent();
+            });
+
+            function raiseEvent() {
+                if (window.getSelection().toString().length && selectstart) {
+                    selectstart = false;
+                    document.dispatchEvent(event);
+                }
+            };
+        };
+        onSelectEnd();
+
+        document.addEventListener('selectend',function(){
+            document.execCommand('copy');  
+            alertify.success("Copied");
+        });
+
+    }+')()';
+
+    var cuteAlerts = document.createElement('script');
+    cuteAlerts.src = 'https://cdn.rawgit.com/alertifyjs/alertify.js/v1.0.10/dist/js/alertify.js';
+    document.head.appendChild(cuteAlerts);
+
+    var inPageScriptNode = document.createElement('script');
+    inPageScriptNode.textContent = inPageScript;
+    document.body.appendChild(inPageScriptNode);
+};
+
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
     var data = request.data || {};
 
     switch (data) {
         case 'rLBclick' :
             removeLock(); 
-            break
+            autoCopy();
+            break;
     }
 
     sendResponse({data: data, success: true});
--- scripts/extension.js
+++ scripts/extension.js
@@ -1,15 +1,22 @@
 var removeLockBtn = document.getElementById('removeLock');
+var copyBtn = document.getElementById('copyLock');
 var statusBar = document.getElementById('status');
+
+function sendEvent(message){
+    statusBar.textContent = 'Processing...';
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {data: message}, function(response) {
+            statusBar.textContent = 'Done!';
+        });
+    });
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     removeLockBtn.addEventListener('click', function () {
-        statusBar.textContent = 'Processing...';
-        var message = 'rLBclick';
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {data: message}, function(response) {
-                statusBar.textContent = 'Done!';
-            });
-        });
+        sendEvent('rLBclick');
+    });
+   copyBtn.addEventListener('click', function(){
+        sendEvent('cBclick');
     });
 });
 
