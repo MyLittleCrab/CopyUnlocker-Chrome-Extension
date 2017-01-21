f75add04051ca54dc491aa54b0ec964dbd02129d
JustAddAcid <rap4491@yandex.ru>
2017-01-21 20:07:43 +0300

    rationalization

--- scripts/content.js
+++ scripts/content.js
@@ -3,24 +3,19 @@
 function removeLock(){
     document.onselectstart = null;
 
-    var styles = ['userSelect','MozUserSelect','WebkitUserSelect','msUserSelect'],
-        bodystyles = getComputedStyle(document.body),
-        setStyleToBody = function(){
-            var isStyleApplied = false;
-            for (var i = 0; i < styles.length; i ++)
-                if (bodystyles[styles[i]] == 'none'){
-                    document.body.style[styles[i]] = 'text';
-                    isStyleApplied = true;
+    var setStyleToBody = function(){
+            if (getComputedStyle(document.body).userSelect == 'none'){
+                    document.body.style.userSelect = 'text';
+                    return true;
                 }
-            return isStyleApplied;
-        };
+            return false;
+    };
 
-        if (! setStyleToBody()){
-            var els = document.body.getElementsByTagName('*');
-            for (var i = 0; i < els.length; i ++)
-                for (var s = 0; s < styles.length; s ++)
-                    els[i].style[styles[s]] = 'text';
-        };
+    if (! setStyleToBody()){
+        for (let i of document.body.getElementsByTagName('*')){
+            i.style.userSelect='text';
+        }
+    };
 }
 
 
